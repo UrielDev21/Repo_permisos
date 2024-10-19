ca3afee8772b9bea661b40b5fa3c3ccaeea0f32d
Uriel <luevanosaavedra@gmail.com>
2024-10-18 20:43:19 -0600

    Se realizo la conexion con la base de datos

---
+++ Acceso_datos/Funciones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Conexion; 
+
+namespace Acceso_datos
+{
+    public class Funciones
+    {
+        Base b = new Base("localhost", "root", "", "bd_usuario");
+        public string Guardar(string q)
+        {
+            return b.Comando(q);
+        }
+        public DataSet Mostrar(string q, string tabla)
+        {
+            return b.Mostrar(q, tabla);
+        }
+        public string Borrar(string q)
+        {
+            return b.Comando(q);
+        }
+        public string Modificar(string q)
+        {
+            return b.Comando(q);
+        }
+        public string ObtenerDato(string q, string tabla, string campo)
+        {
+            return b.ObtenerDato(q, tabla, campo);
+        }
+    }
+}
